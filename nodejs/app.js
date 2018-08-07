ce3f8771a2eb92314a5e7aaf98ea143bafac2182
MaxySpark <devmaxyspark@gmail.com>
2018-08-08 01:04:06 +0530

    node progress

--- nodejs/app.js
+++ nodejs/app.js
@@ -8,9 +8,32 @@ request.get('https://api.github.com/user/repos', {
     'user': config.github.username,
     'pass': config.github.password
   },
+  json : true,
   headers: {
     'User-Agent': 'nodejs'
   }
 },function(err,res,body){
-    console.log(body);
+
+    let private_repos = _.filter(body,function(repo) { return repo.private === true; });
+
+    let repo_data = [];
+
+    private_repos.forEach(repo => {
+      let model = {
+        'name'      : repo.name,
+        'git_url'   : repo.git_url,
+        'ssh_url'   : repo.ssh_url,
+        'clone_url' : repo.clone_url
+      }
+      repo_data.push(model);
+    });
+
+    
+
+    console.log(repo_data);
+    // body.forEach(element => {
+    //   console.log(element);
+    // });
+    // console.log(typeof(body));
+    // console.log(JSON.parse(body));
 });
