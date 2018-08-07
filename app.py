040a9062877839120a98cdc5f4d4598730748378
MaxySpark <devmaxyspark@gmail.com>
2018-08-07 20:01:30 +0530

    python init

--- app.py
+++ app.py
@@ -1,8 +1,21 @@
 import requests
+import argparse
+import json
+parser = argparse.ArgumentParser()
+parser.add_argument("username", help="Github Username")
+parser.add_argument("password", help="Github Password")
+args = parser.parse_args()
+# print(args.username)
 
-username = ""
-password = ""
+username = args.username or ""
+password = args.password or ""
 
-r = requests.get('https://api.github.com/user', auth=(username, password))
-print(r)
-print(r.json())
+r = requests.get('https://api.github.com/user/repos', auth=(username, password))
+# print(r)
+# print(r.json())
+# print(json.dumps(r.json()))
+
+for repo in r.json():
+    print(repo["git_url"])
+
+# print(json.dumps(r.json()))
